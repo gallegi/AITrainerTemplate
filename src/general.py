b0ae35387b7acb87d979e4e877a5f60497b92948
Nguyen The Nam <namnguyen61031@gmail.com>
2024-05-01 08:45:42 +0200

    update seed_everything to make deberta v3 deterministic

--- src/general.py
+++ src/general.py
@@ -18,6 +18,7 @@ def seed_everything(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
 
 def get_logger(name, path, mode='a'):
     logger = logging.getLogger(name)  
